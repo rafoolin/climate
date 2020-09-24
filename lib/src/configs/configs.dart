f6bad8ff6c4a9c47472e31a294c47d165fc0b40f
Rafoolin <niloo.jv@gmail.com>
2020-09-24 15:01:39 +0200

    Added THEME template

--- lib/src/configs/configs.dart
+++ lib/src/configs/configs.dart
@@ -1,3 +1,4 @@
 export 'package:climate/src/configs/custom_color.dart';
 export 'package:climate/src/configs/custom_ratio.dart';
 export 'package:climate/src/configs/custom_preferences.dart';
+export 'package:climate/src/configs/theme.dart';
