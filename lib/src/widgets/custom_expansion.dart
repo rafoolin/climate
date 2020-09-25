3917256234a3f7580655a7356ad83d786c7f628c
Rafoolin <niloo.jv@gmail.com>
2020-09-25 23:18:51 +0200

    Used Animation for expansion

--- lib/src/widgets/custom_expansion.dart
+++ lib/src/widgets/custom_expansion.dart
@@ -1,3 +1,4 @@
+import 'package:climate/src/blocs/blocs.dart';
 import 'package:flutter/material.dart';
 
 class CustomExpansion extends StatefulWidget {
@@ -9,13 +10,31 @@ class CustomExpansion extends StatefulWidget {
   _CustomExpansionState createState() => _CustomExpansionState();
 }
 
-class _CustomExpansionState extends State<CustomExpansion> {
+class _CustomExpansionState extends State<CustomExpansion>
+    with SingleTickerProviderStateMixin {
   bool _isExpanded = false;
+  Animation<double> _size;
+  AnimationController _controller;
+  PreferencesBloc _bloc;
+  @override
+  void initState() {
+    _bloc = BlocProvider.of<PreferencesBloc>(context);
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+
+    _size = Tween(
+            begin: 3 * widget.itemExtent,
+            end: widget.itemExtent * (widget.children.length + 1))
+        .animate(_controller);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     print('CustomExpansion');
-    return SizedBox.fromSize(
+
+    return AnimatedBuilder(
+      animation: _controller,
       child: Stack(
         children: [
           Positioned.fill(
@@ -24,7 +43,8 @@ class _CustomExpansionState extends State<CustomExpansion> {
             child: ListView.builder(
               itemExtent: widget.itemExtent,
               physics: NeverScrollableScrollPhysics(),
-              itemCount: _isExpanded ? widget.children.length : 3,
+              scrollDirection: Axis.vertical,
+              itemCount: widget.children.length,
               shrinkWrap: true,
               itemBuilder: (context, index) => widget.children[index],
             ),
@@ -33,31 +53,50 @@ class _CustomExpansionState extends State<CustomExpansion> {
             left: 0.0,
             right: 0.0,
             bottom: 0.0,
-            child: FlatButton(
-              hoverColor: Colors.grey.shade200.withOpacity(.3),
-              color: Theme.of(context).brightness == Brightness.dark
-                  ? Colors.black26
-                  : Colors.grey.shade200.withOpacity(.35),
-              onPressed: () {
-                setState(() {
-                  _isExpanded = !_isExpanded;
-                });
+            child: StreamBuilder(
+              stream: _bloc.themeStream,
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    return FlatButton(
+                      hoverColor: Colors.grey.shade200.withOpacity(.3),
+                      color: snapshot.data
+                          ? Colors.black26
+                          : Colors.grey.shade200.withOpacity(.35),
+                      onPressed: () {
+                        if (_controller.status == AnimationStatus.completed) {
+                          _isExpanded = false;
+                          _controller.reverse();
+                        } else if (_controller.status ==
+                            AnimationStatus.dismissed) {
+                          _isExpanded = true;
+                          _controller.forward();
+                        }
+                        setState(() {});
+                      },
+                      child: Icon(
+                        _isExpanded
+                            ? Icons.keyboard_arrow_up
+                            : Icons.keyboard_arrow_down,
+                        color: Colors.grey,
+                      ),
+                    );
+                    break;
+                  default:
+                    return Container();
+                }
               },
-              child: Icon(
-                _isExpanded
-                    ? Icons.keyboard_arrow_up
-                    : Icons.keyboard_arrow_down,
-                color: Colors.grey,
-              ),
             ),
           )
         ],
       ),
-      size: Size.fromHeight(widget.itemExtent *
-              (_isExpanded
-                  ? _isExpanded ? widget.children.length : 3 + 1
-                  : 3.0) +
-          36.0),
+      builder: (context, child) {
+        return SizedBox.fromSize(
+          child: child,
+          size: Size.fromHeight(_size.value),
+        );
+      },
     );
   }
 }
